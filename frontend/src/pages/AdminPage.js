f43903a4cddeb011ab3b9d71082f691cb0ca1ab8
MOOUUAAD <143042089+MOOUUAAD@users.noreply.github.com>
2024-11-24 23:21:27 +0100

    Update AdminPage.js

--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import io from 'socket.io-client';  // Import socket.io-client
 
 const AdminPage = () => {
   const [adminKey, setAdminKey] = useState('');
